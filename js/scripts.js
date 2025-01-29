f001b1b958f9d759dcd1cc0832589f6a71fbcdc7
Jan Trimel <j.trimel@gmail.com>
2025-01-29 17:03:29 +0100

    add canva portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,25 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            const headerDiv = document.getElementById("header");
+            headerDiv.innerHTML = data;
+
+            // Example: Add a class based on the current page
+            if (window.location.pathname.includes("index.html")) {
+                document.getElementById("nav-link-home").classList.add("active");
+                document.getElementById("nav-link-galery").classList.remove("active")
+            } else if (window.location.pathname.includes("products.html")) {
+                document.getElementById("nav-link-galery").classList.add("active");
+                document.getElementById("nav-link-home").classList.remove("active")
+            } else {
+                headerDiv.classList.add("default-header");
+            }
+        })
+        .catch(error => console.error("Error loading header:", error));
+});
